40965bf90dd6b147d08b90b6062818b25cfc9646
wy <wyawxy@gmail.com>
2016-09-18 01:55:59 +0100

    e

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+var TodoComponent = Vue.extend({
+	data:function(){
+		return {
+			inEditMode:false
+		}
+	},
+	props:['todo'],
+	template: '<div><span v-on:click="clicked" v-show="!inEditMode">{{todo.title}}</span><input v-on:keyup.enter="save" v-model="todo.title" v-show="inEditMode" /></div>',
+	methods:{
+		clicked: function (){
+			this.inEditMode = true;
+		},
+		save: function(){
+			this.inEditMode = false;
+		}
+	}
+});
+
+Vue.component('todo-component', TodoComponent);
+
 new Vue({
 	el: '#app',
 	data: {
